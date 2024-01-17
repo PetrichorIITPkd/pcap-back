727ac0ccaa286290e3bcb65208608b0846cf0230
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2024-01-17 15:37:54 +0530

    models reverted

--- app/models.py
+++ app/models.py
@@ -6,30 +6,19 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 User=get_user_model()
 
-
-class Institute(models.Model):
-    institutionType = models.CharField(max_length=255, null=True)
-    instiName = models.CharField(unique=True, max_length=255)
-    def __str__(self):
-        return self.instiName
-
-
 class Profile(models.Model):
     # objects = None
 
     def generate_CA(self):
         char_list = '1234567890abcdefghijklmnopqrstuvwxyz'
         self.CA = ''.join(random.choice(char_list) for _ in range(6))
-    # userId=models.IntegerField()
+    userId=models.IntegerField()
     username = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=13)
+    college = models.CharField(max_length=100, null=True)
+    year = models.CharField(max_length=5, null=True)
     CA = models.TextField(null=True)
-    instituteID = models.CharField(null=True, max_length=255)
-    joinYear = models.CharField(max_length=5, null=True)
-    gradYear = models.IntegerField(null=True)
-    stream = models.CharField(null=True, max_length=10)
-
 
     def __str__(self):
         return self.username
