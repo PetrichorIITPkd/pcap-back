5bb56964511baf13e784429e9ff1484c8b95318f
alonot <priyanshup27092004@gmail.com>
2024-01-24 23:05:29 +0530

    password fix

--- app/views.py
+++ app/views.py
@@ -158,12 +158,12 @@ def admin_data(request):
     
     data = request.data
     password = data["password"]
-    # if "PWD" not in settings.os.environ:
-    #     return Response({
-    #         "status": 500,
-    #         "message": "Password not set at host, Contact someone idk."
-    #     })
-    if password == "password":
+    if "PWD" not in settings.os.environ:
+        return Response({
+            "status": 500,
+            "message": "Password not set at host, Contact someone idk."
+        })
+    if password == settings.os.environ["password"]:
         try:
             AllData=[]
             for ca in Profile.objects.all():
